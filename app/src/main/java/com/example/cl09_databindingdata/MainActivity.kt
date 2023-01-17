5974c807e83d50dfe544c10c114b696296db6672
José Francisco Jarquin Martínez <ex500791.jose.jarquin@mail.telcel.com>
2023-01-17 12:21:39 -0600

    ya quedó

--- app/src/main/java/com/example/cl09_databindingdata/MainActivity.kt
+++ app/src/main/java/com/example/cl09_databindingdata/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.cl09_databindingdata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.cl09_databindingdata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        var usuario = Usuario()
+        usuario.nombre = "Miguel"
+        usuario.email = "info@miguelcampos.dev"
+
+        binding.user = usuario
     }
 }
---
+++ app/src/main/java/com/example/cl09_databindingdata/Usuario.kt
@@ -0,0 +1,6 @@
+package com.example.cl09_databindingdata
+
+data class Usuario(
+    var nombre: String = "",
+var email: String = ""
+)
